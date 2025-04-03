0cd53395c502f25a994fcacc889a576a3e09fcad
Grant-Gordon <ggmain777@gmail.com>
2025-04-02 21:44:46 -0400

    think i got a basic dataset class working that should fit well with the dataloader class. Figured out I can actually convert to dense on __getitem__ as in convert ONLY the sample to dense and never actually need to load a npz into dense... not sure exaxtly what to do with this info but it gives me hope for optimization later down the road

---
+++ dataLoader/single_chunk_dataset.py
@@ -0,0 +1,61 @@
+import torch
+from torch.utils.data import Dataset
+import scipy.sparse as sp
+import pickle
+import numpy as np
+import os
+import random 
+
+
+
+class ChunksDataset:
+    def __init__(self, data_dir_path, target_species = "human"):
+        self.data_dir_path = data_dir_path
+        self.shuffled_chunk_list = self._get_shuffled_chunk_list()
+        self.target_species = target_species
+        self.current_chunk_idx = -1 
+
+    #returns a shuffled list of tuples(count, metadata) file names
+    def _get_shuffled_chunk_list(self):
+        chunk_list = []
+
+        files = set(os.listdir(self.data_dir_path))
+
+        for file in files:
+            if file.startswith(f"count_{self.target_species}_"):
+                metadata_file = file.replace("count", "metadata")
+                metadata_file = metadata_file.replace("npz", "pkl")
+                if metadata_file in files: 
+                    chunk_list.append((file,metadata_file))
+        random.shuffle(chunk_list)
+        return chunk_list
+    
+    def __getitem__(self, index):
+        return self.shuffled_chunk_list[index]
+    
+    def __len__(self):
+        return len(self.shuffled_chunk_list)
+
+
+
+class SingleChunkDataset(Dataset):
+    
+    def __init__(self, counts_path, metadata_path):
+        self.counts_path = counts_path
+        self.metadata_path = metadata_path
+
+        with open(self.counts_path, "rb") as f:
+            self.counts_csr = sp.load_npz(f)
+        self.samples_in_chunk = self.counts_csr.shape[0]
+
+
+        with open(self.metadata_path, "rb") as f:
+            self.metadata = pickle.load(f)
+
+
+    def __getitem__(self, index):
+        return torch.tensor(self.counts_csr[index].toarray(), dtype=torch.float32)  # Convert row to dense
+    
+
+    def __len__(self):
+        self.samples_in_chunk
