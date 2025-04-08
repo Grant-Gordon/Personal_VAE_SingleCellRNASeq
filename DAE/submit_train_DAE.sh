eefbd86ff721f040b060297ee09e01b29104ac49
Grant-Gordon <ggmain777@gmail.com>
2025-04-08 18:46:22 -0400

    Think i've got the logic flushed out for parallelized preloading of chunks into ram, and parallelized batch creations (Dataloader num_workers, pin_memmory). Time to test out slurm

--- DAE/submit_train_DAE.sh
+++ DAE/submit_train_DAE.sh
@@ -6,19 +6,34 @@
 #SBATCH --gpus-per-node=2
 #SBATCH --mem=32G
 
-WORKING_DIR="/mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq"
-DATA_DIR="/mnt/projects/debruinz_project/july2024_census_data/full"
-OUTPUT_DIR="/mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq/JobOutputs"
+#Define environment variables 
+PROJECT_ROOT_DIR="/mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq"
+DATA_DIR="/mnt/projects/debruinz_project/july2024_census_data/subset" #Use data/subset for 3_mil samples use data/full for full 16 mill census data
+OUTPUTS_DIR="/mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq/JobOutputs"
+RUN_OUTPUT_DIR="${OUTPUTS_DIR}/job_${SLURM_JOB_ID}-${SLURM_JOB_NAME}"
+mkdir -p $RUN_OUTPUT_DIR
 
+
+
+#Model Paramters 
 EPOCHS=1
 BATCH_SIZE=128
 SPECIES="human"
+CHUNKS_PRELOADED=2
+NUM_PRELOADER_THREADS=2
 
+#Set up environment 
 cd "$WORKING_DIR" || exit 1
 source personalVAEenv/bin/activate
 
+#Train Model 
 export PYTHONPATH="$WORKING_DIR:$PYTHONPATH"
-python3 -m DAE.train_DAE --data_dir=$DATA_DIR --epochs=$EPOCHS --batch_size=$BATCH_SIZE --output_dir=$OUTPUT_DIR --species=$SPECIES 
-
+python3 -m DAE.train_DAE --data_dir=$DATA_DIR --epochs=$EPOCHS --batch_size=$BATCH_SIZE --output_dir=$OUTPUT_DIR --species=$SPECIES --chunks_preloaded=$CHUNKS_PRELOADED --num_preloader_threads=$NUM_PRELOADER_THREADS
+#clean up output 
+mv *.log $RUN_OUTPUT_DIR
+mv *.out $RUN_OUTPUT_DIR
+mv *.csv $RUN_OUTPUT_DIR
+mv *.err $RUN_OUTPUT_DIR
+mv tensorboard_logs $RUN_OUTPUT_DIR
 
 
--- DAE/train_DAE.py
+++ DAE/train_DAE.py
@@ -7,12 +7,31 @@ import argparse
 import os
 import time
 import csv
+import threading
+from queue import Queue
 
 from dataLoader.single_chunk_dataset import SingleChunkDataset, ChunksDataset
 from DAE.dae import DeepAutoEncoder
 
-def main():
+def create_dataloader(data_dir, counts_file, metadata_file, batch_size):
+    dataset = SingleChunkDataset(
+        counts_path=os.path.join(data_dir, counts_file),
+        metadata_path=os.path.join(data_dir, metadata_file)
+    )
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,      #TODO tweak to optimize performacne
+        pin_memory=True     #avoid memory duplication when mulitprocessing on GPU TODO(look into docs)
+    )   
 
+    return dataloader
+    
+
+
+def main():
+    main_start_time = time.time()
     parser = argparse.ArgumentParser(description="Train Deep AutoEncoder on single-cell RNA chunks.")
     parser.add_argument("--data_dir", type=str, default="../../july2024_census_data/full/", help="Path to dataset directory.")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs.")
@@ -20,7 +39,8 @@ def main():
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate.")
     parser.add_argument("--output_dir", type=str, default="./", help="Directory to save trained model.")
     parser.add_argument("--species",type=str, default="human",  help = "the target species that the the dataset will be comprised of NOTE file name convention is hardcoded")
-   
+    parser.add_argument("--chunks_preloaded", type=int, default=1,help="The number of chunks the dataloader will attempt to pre-load into RAM via background threads (preload_queue.size =chunks_prelaoded)")
+    parser.add_argument("--num_preloader_threads", type=int, default=1, help="The number of threads that will be attemping to preload chunks into RAM:")
     args = parser.parse_args()
    
     data_dir = args.data_dir
@@ -29,21 +49,17 @@ def main():
     lr = args.lr
     output_dir = args.output_dir
     species = args.species
+    chunks_preloaded = args.chunks_preloaded
+    num_preloader_threads = args.num_preloader_threads
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #TODO is esle neccessary? dont want to run on cpu anyway 
     # Initialize dataset
     chunks_dataset = ChunksDataset(data_dir_path=data_dir, target_species=species)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #TODO is esle neccessary? dont want to run on cpu anyway 
-
-    # Extract input size dynamically from the first chunk
+    # Extract input size from  the first chunk
     first_counts_file, first_metadata_file = chunks_dataset[0]
-
-    first_counts_path = os.path.join(data_dir, first_counts_file)
-    first_metadata_path = os.path.join(data_dir, first_metadata_file)
-
-    first_chunk_dataset = SingleChunkDataset(counts_path=first_counts_path, metadata_path=first_metadata_path)
-    input_size = first_chunk_dataset[0].shape[0]  # Assuming each sample is 1D (genes/features)
-
+    input_size = create_dataloader(data_dir, first_counts_file, first_metadata_file, 1).dataset[0].shape[0] #createdatalaoder = dl, dl.dataset = SingleChunk, SingelChunk[0] = counts[0].array.flatten tensor
+  
     # Initialize model, optimizer, and loss function
     model = DeepAutoEncoder(input_size).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -55,45 +71,86 @@ def main():
     timing_log_path = os.path.join(output_dir, "dataloader_timing_log.csv")
     log_file = open(timing_log_path, 'w', newline="")
     csv_writer = csv.writer(log_file)
-    csv_writer.writerow(["epoch", "chunk", "batch", "chunk_load_time", "batch_train_time"])
+    csv_writer.writerow([
+        "epoch",
+        "chunk",
+        "batch",
+        "chunk_load_time",
+        "batch_creation_time" 
+        "batch_train_time"])
+
+    #prepare for multithreaded pre-loading of chunks
+    preload_queue = Queue(maxsize=chunks_preloaded)
+    chunk_queue= Queue()
+    def preload_worker():
+        while True:
+            try:
+                chunk_idx = chunk_queue.get(timeout=3) #Wait up to 3 seconds for work
+            except:
+                break # exit thread if no more work 
+            
+            chunk_start_time = time.time()
+            counts_file, metadata_file = chunks_dataset[chunk_idx]
+            dataloader = create_dataloader(data_dir=data_dir, counts_file=counts_file, metadata_file=metadata_file, batch_size=batch_size)
+            chunk_load_time = time.time() - chunk_start_time
+            
+            preload_queue.put((chunk_idx, dataloader, chunk_load_time)) # blocks if Q full 
+            chunk_queue.task_done()
+
+    #populate Q with chunks
+    for i in range(len(chunks_dataset)):
+        chunk_queue.put(i)
+
+    for _ in range(num_preloader_threads):
+        t = threading.Thread(target= preload_worker)
+        t.daemon=True
+        t.start()
 
 
+    print(f"Time until First Epoch reached: {time.time() - main_start_time:.2f}s")
     for epoch in range(epochs):
+        epoch_start_time = time.time()
+
         epoch_loss = 0.0
         num_batches = 0
 
-        for chunk_idx, (counts_file, metadata_file) in enumerate(chunks_dataset):  # Iterate over each chunk (lazy loading)
-            chunk_start_time = time.time()
-            single_chunk_dataset = SingleChunkDataset(counts_path=os.path.join(data_dir, counts_file), metadata_path=os.path.join(data_dir, metadata_file))
-            dataloader = DataLoader(single_chunk_dataset, batch_size=batch_size, shuffle=True)
-            chunk_load_time = time.time() - chunk_start_time
+        for chunk_idx in range(len(chunks_dataset)):
+            loaded_chunk_idx, current_loader, chunk_load_time = preload_queue.get()
 
-            for batch_idx, batch in enumerate(dataloader):
+            assert loaded_chunk_idx == chunk_idx,  f"Expected chunk {chunk_idx}, got {loaded_chunk_idx}"
+
+
+            #Training step 
+            for batch_idx, batch in enumerate(current_loader):
+                batch_creation_start_time = time.time()
                 batch = batch.to(device)
+                batch_creation_time  = time.time() - batch_creation_start_time
 
                 batch_start_time = time.time()
-
                 optimizer.zero_grad()
                 outputs = model(batch)
                 loss = criterion(outputs, batch)
                 loss.backward()
                 optimizer.step()
-                
                 batch_train_time = time.time() - batch_start_time
 
                 csv_writer.writerow([
-                    epoch,chunk_idx,
+                    epoch,
+                    chunk_idx,
                     batch_idx,
                     f"{chunk_load_time:.6f}",  
-                    f"{batch_train_time:.6f}"
+                    f"{batch_creation_time:.6f}",
+                    f"{batch_train_time}"
                     ])
 
                 epoch_loss += loss.item()
                 num_batches += 1  # Track number of batches for averaging
+            preload_queue.task_done() #optional???
 
         avg_loss = epoch_loss / num_batches if num_batches > 0 else float("inf")
         writer.add_scalar("Loss/train", avg_loss, epoch)
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss:.6f}")
+        epoch_time = time.time() - epoch_start_time
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {avg_loss:.6f}, Epoch Time: {epoch_time:.2f}s")
 
     # Save the model
     model_save_path = os.path.join(output_dir, "dae_model.pth")
