62a5a1e95d05d7a7ff6f618fda4528fe2f7c13cf
Grant-Gordon <ggmain777@gmail.com>
2025-07-11 11:20:05 -0400

    changed return type to dict for ease of use

--- cpp_CVAE/front_end_nightly/single_chunk_loader.py
+++ cpp_CVAE/front_end_nightly/single_chunk_loader.py
@@ -3,14 +3,16 @@ from scipy import sparse
 import numpy as np
 import pickle #TODO
 
-
 def load_csr_pointers(counts_path):
-
     with open(counts_path, 'rb') as f:
         counts_csr = sparse.load_npz(f)
-
-
-    return (counts_csr.data, counts_csr.indices, counts_csr.indptr, counts_csr.shape, counts_csr.nnz)
+    return {
+        "data":counts_csr.data,
+        "indices":counts_csr.indices,
+        "indptr":counts_csr.indptr,
+        "shape":counts_csr.shape,
+        "nnz":counts_csr.nnz
+        }
 
 
 
