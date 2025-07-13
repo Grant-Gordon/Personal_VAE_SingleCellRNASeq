b5ee20ddc1d2f3712b91b104eedbfbba5d71c51f
Grant-Gordon <ggmain777@gmail.com>
2025-07-13 12:32:23 -0400

    added the script that will parse the config and generate constexpr\'s for the config values that are stored in a config.h file

---
+++ cpp_CVAE/front_end_nightly/example_config.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <string_view>
+#include <memory>
+#include <array>
+#include "Layers/LinearLayer.hpp"
+#include "Layers/ReLULayer.hpp"
+#include "param_init_util.h"
+
+namespace config {
+constexpr int              Global__seed = 0;
+constexpr bool             Global__using_metadata = false;
+constexpr std::string_view Global__scalar = float;
+constexpr int              Model__latent_dim = 128;
+constexpr int              Training__epochs = 1;
+constexpr int              Training__batch_size = 128;
+constexpr flaot            Training__lr = 0.001;
+constexpr std::string_view Training__output_dir = /mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq/CVAE/JobOutputs;
+constexpr int              Training_Optimizer__beta1 = 1;
+constexpr int              Training_Optimizer__beta2 = 2;
+constexpr int              Training_Optimizer__epsilon = 3;
+constexpr std::string_view Data__data_dir = /mnt/projects/debruinz_project/july2024_census_data/subset;
+constexpr std::string_view Data__counts_file_pattern = human_counts_*.npz;
+constexpr std::string_view Data__metadata_file_pattern = human_metadata_*.pkl;
+constexpr std::string_view Data__vocab_builder_out = /mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq/CVAE/preprocessed_data/;
+constexpr std::string_view Data__species = human;
+constexpr int              Data__chunks_to_preload = 0;
+constexpr int              Data__batches_to_prelaod = 0;
+constexpr std::string_view Data__num_features = 999;
+constexpr std::string_view ConditionalHeads__metadata_vocab = /mnt/projects/debruinz_project/grant_gordon/Personal_VAE_SingleCellRNASeq/CVAE/preprocessed_data/human_vocab_dict.pkl;
+constexpr std::string_view ConditionalHeads_MetadataFields_Assay__type = onehot;
+constexpr std::string_view ConditionalHeads_MetadataFields_AssayOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_CellType__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_CellType__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_CellTypeOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_DatasetId__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_DatasetId__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_DevStage__type = onehot;
+constexpr std::string_view ConditionalHeads_MetadataFields_DevelopmentStage__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_DevelopmentStage__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_DevelopmentStageOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_Disease__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_Disease__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_DiseaseOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_DonorId__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_DonorId__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_IsPrimaryData__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_NMeasuredVars__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_Nnz__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_ObservationJoinid__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_RawMeanNnz__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_RawSum__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_RawVarianceNnz__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_SelfReportedEthnicity__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_SelfReportedEthnicity__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_SelfReportedEthnicityOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_Sex__type = onehot;
+constexpr std::string_view ConditionalHeads_MetadataFields_SexOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_SomaJoinid__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_SuspensionType__type = onehot;
+constexpr std::string_view ConditionalHeads_MetadataFields_Tissue__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_Tissue__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_TissueGeneral__type = embedding;
+constexpr int              ConditionalHeads_MetadataFields_TissueGeneral__embedding_dim = 128;
+constexpr std::string_view ConditionalHeads_MetadataFields_TissueGeneralOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_TissueOntologyTermId__type = IGNORE;
+constexpr std::string_view ConditionalHeads_MetadataFields_TissueType__type = IGNORE;
+
+template <typename Scalar>
+constexpr std::array<std::shared_ptr<Layer<Scalar>>, 7> layers_vector = {
+    std::make_shared<LinearLayer<Scalar>>(999, 512, glorot),
+    std::make_shared<RELULayer<Scalar>>(),
+    std::make_shared<LinearLayer<Scalar>>(512, 128, florot),
+    std::make_shared<RELULayer<Scalar>>(),
+    std::make_shared<LinearLayer<Scalar>>(128, 512, glorot),
+    std::make_shared<RELULayer<Scalar>>(),
+    std::make_shared<LinearLayer<Scalar>>(512, 999, glorot),
+};
+}
